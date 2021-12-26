e2e2deb2b2849eaaaff3144dba19c01a129b7e70
김시훈 <ch10152080@gmail.com>
2021-12-26 16:21:11 +0900

    Structure

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,21 @@
-import UIKit
+struct Town {
+    let name = "Angelalend"
+    var citizens = ["Angela","Jack Bauer"]
+    var resources = ["Grain": 100, "Ore": 42, "Wool": 75]
+    
+    func fortufy() {
+        print("Defences increased!")
+    }
+}
 
-var greeting = "Hello, playground"
+var myTown = Town()
+
+print(myTown.citizens)
+print("\(myTown.name) has \(myTown.resources["Grain"]!) bag of grain")
+
+
+myTown.citizens.append("Keanu Reeves")
+print(myTown.citizens.count)
+
+
+myTown.fortufy()
